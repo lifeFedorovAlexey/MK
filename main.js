817697bce74f9f1ae4631869f2ac1816cd4d2dc5
alexeyFedorov <lifelex2011@gmail.com>
2021-10-13 13:36:52 +0300

    refactoring

--- main.js
+++ main.js
@@ -1,45 +1,72 @@
-class Player {
-  constructor(id, name, img, hp) {
+const IMAGE_URL = "http://reactmarathon-api.herokuapp.com/assets/";
+const DEFAULT_IMAGE_URL = IMAGE_URL + "subzero.gif";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initGame();
+});
+
+initGame = () => {
+  let game = {};
+  game.arena = new Arena("arena");
+  game.arena.addPlayers([
+    { name: "SUB-ZERO", hp: 50 },
+    { name: "SONYA", hp: 80 },
+  ]);
+};
+
+class Arena {
+  constructor(id) {
     this.id = id;
+    this.container = this.getContainer();
+    this.players = [];
+  }
+  getContainer = () => {
+    return document.getElementById(this.id);
+  };
+
+  addPlayers = (players) => {
+    this.players = [...players];
+    this.players.map(({ name, hp }, id) => {
+      this.createPlayer(id, name, hp);
+    });
+  };
+
+  createPlayer = (id, name, hp) => {
+    const player = new Player(id, name, hp);
+    let playerComponent = document.createElement("div");
+    playerComponent.classList.add(`player${player.id}`);
+    playerComponent.innerHTML = `
+          <div class="progressbar">
+              <div class="life"></div>
+              <div class="name">${player.name}</div>
+          </div>
+          <div class="character">
+              <img src="${player.img}" onerror="onPlayerImageError(this)"/>
+          </div>`;
+    this.container.appendChild(playerComponent);
+  };
+}
+
+class Player {
+  constructor(id, name, hp) {
+    this.id = ++id;
     this.name = name;
     this.hp = hp;
-    this.img = img;
+    this.img = this.getImgFileName(name);
     this.weapon = ["leftLeg", "rightHand"];
   }
 
   attack = () => {
     console.log(this.name, "Fight...");
   };
+
+  getImgFileName = () => {
+    return (
+      IMAGE_URL + this.name.replaceAll(/[^a-zA-Z]+/g, "").toLowerCase() + ".gif"
+    );
+  };
 }
 
-createPlayer = (id, name, img, hp) => {
-  const player = new Player(id, name, img, hp);
-  const arena = document.getElementById("arena");
-  let playerComponent = document.createElement("div");
-  playerComponent.classList.add(`player${player.id}`);
-  playerComponent.innerHTML = `
-      <div class="progressbar">
-          <div class="life"></div>
-          <div class="name">${player.name}</div>
-      </div>
-      <div class="character">
-          <img src="${player.img}" />
-      </div>`;
-  arena.appendChild(playerComponent);
+onPlayerImageError = (element) => {
+  element.src = DEFAULT_IMAGE_URL;
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-  createPlayer(
-    1,
-    "subZero",
-    "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
-    50
-  );
-
-  createPlayer(
-    2,
-    "sonya",
-    "http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
-    80
-  );
-});
